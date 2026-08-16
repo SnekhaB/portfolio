ec77ca50207a86bb6f0fedd31d4cb7989fee55ef
snekha-baskar <snekhabaskar@neorains.com>
2026-08-16 10:43:28 +0530

    fix: config file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/portfolio/" : "/",
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-});
+}));
